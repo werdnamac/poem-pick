bea2d037dbc28d6e18402265e6df8245a881bcdf
Andy Watson <werdnamac@gmail.com>
2022-07-31 20:53:09 -0400

    Tidied up the code a little. Added some comments, removed some console.logs, and rearranged code a littl here and there to keep code blocks together.

--- js/script.js
+++ js/script.js
@@ -6,7 +6,7 @@ const filter = document.querySelector(".filter");
 const wildAuthor = document.querySelector(".wild-1");
 // variable for the wild card button to choose the title name
 const wildTitle = document.querySelector(".wild-2");
-// variable for the Start Over button
+// variable for the "Forget This. Start Over" button
 const backHome = document.querySelector(".back-home");
 // variable to select the section with the search bar, the buttons, and the unordered list
 const authorsOrtitles = document.querySelector(".authors-or-titles");
@@ -14,7 +14,7 @@ const authorsOrtitles = document.querySelector(".authors-or-titles");
 const list = document.querySelector(".list");
 // variable to select the download button
 const downloadPoem = document.querySelector(".download");
-//variable to select the start over button
+//variable to select the Start Over button on the poem display page
 const startOver = document.querySelector(".start-over");
 //variable to select the div that holds both (1) the final-screen buttons and (2) the div where we display the poem
 const poemAndButtons = document.querySelector(".poem-and-buttons");
@@ -58,7 +58,7 @@ const displayAuthorNames = function(authorNames) {
         
     }
     // Set the initial message
-    message.innerHTML = "Search for and Select an Author's Name. Or push the <strong>Wild Card</strong> button, and we'll select.";
+    message.innerHTML = "Select an Author's Name. Or push the <strong>Wild Card</strong> button, and we'll select one.";
     
 }
 
@@ -76,19 +76,22 @@ filter.addEventListener("input", function (e) {
     const searchText = e.target.value;
     // turn the captured text to lower case
     const lowerSearchText = searchText.toLowerCase();
-    //select all repos in document
+    //select all the author names and title names in document
     const searchables = document.querySelectorAll(".author-item, .title-item");
   
     for (const searchable of searchables) {
-      // looping through all the author or title items, capturing lower case versions of all inner text
+      // looping through all the author and title items, capturing lower case versions of all inner text
       const lowerSearchable = searchable.innerText.toLowerCase();
-      // only show repos that include some of the searched text
+      // only show items that include some of the searched text 
+      // we already have the list showing either only author names or title names
+      // so combined with the below code block, we now show only the searched for
+      // items within the relevant category (either author names or title names)
       if (lowerSearchable.includes(lowerSearchText)) {
        searchable.classList.remove("hide");
       }
         else {
           searchable.classList.add("hide");
-        } // if else clause ends here
+       } // if else clause ends here
       } //for look ends here
     });
 
@@ -107,14 +110,18 @@ list.addEventListener("click", function(e) {
 
     //get the names of the poems written by the selected author
   getTitleNames = async function(selectedAuthor) {
+      // the :abs in the fetch url specifies an exact match 
+      // (as opposed to just containing the sought string)
       const titlesRequest = await fetch(`https://poetrydb.org/author/${selectedAuthor}:abs`);
       titles = await titlesRequest.json();
-      //displayTitles(titles);
-      console.log(selectedAuthor);
-      console.log(titles)
-;      displayTitles(titles)
+      
+     
+    //hides the authors' Wild Card button and unhides the titles' Wild Card button
       wildAuthor.classList.add("hide");
       wildTitle.classList.remove("hide");
+
+      //send the selected title to the displayTitles function
+      displayTitles(titles)
       
   }
 
@@ -131,7 +138,7 @@ const displayTitles = function(titles) {
         titleItem.innerHTML = `<h5>${title.title}</h5>`
         list.append(titleItem);
     }
-    message.innerHTML=`Select a title by ${selectedAuthor}`;
+    message.innerText=`Select a title by ${selectedAuthor}`;
 }   
 
   // pick random title if user clicks on the wild card when on the title page
@@ -144,9 +151,9 @@ const displayTitles = function(titles) {
     getPoem(randomTitle);
 });
 
+//go back to the start if the Forget This. Start Over button is clicked.
+// (That button is visible only on the page with the list of titles
 backHome.addEventListener("click", () => {
-
-    console.log("hi");
 
     //hide the button configuration for the page displaying the titles 
     wildTitle.classList.add("hide");
@@ -175,7 +182,7 @@ backHome.addEventListener("click", () => {
 });
 
 getPoem = async function(selectedTitle) {
-    
+    //the :abs selects for exact matches (as opposed to partial string matches)
     const poemRequest = await fetch(`https://poetrydb.org/title,author/${selectedTitle}:abs;${selectedAuthor}:abs/author,title,lines.text`);
     const poem = await poemRequest.text();
 
@@ -196,17 +203,17 @@ displayPoem = function(poem) {
         console.log(titles);
     }
 
+    wildTitle.classList.add("hide");
+
+    message.innerHTML = "" 
+
     const splitPoem = poem.split("\n");
     console.log(splitPoem);
-
-    wildTitle.classList.add("hide");
 
     const thisTitle = splitPoem[1];
     const thisAuthor = splitPoem[3];
     const thisText = splitPoem.splice(5, splitPoem.length);
-    thisTextFormatted = thisText.join(" <br> ")
-    
-    message.innerHTML = ""   
+    thisTextFormatted = thisText.join(" <br> ")  
 
     const poemDiv = document.createElement("div");
 
@@ -222,20 +229,13 @@ displayPoem = function(poem) {
     </div> 
     `
     thePoem.append(poemDiv);
-
-    if (thisAuthor.includes(`${selectedAuthor}`)) {
-        console.log("match");
-    } else {
-        console.log("no match");
-    }
-    /*}*/
-
-   
+  
 }
 
 function download(file, text) {
               
-    //creating an invisible element
+    //creating an invisible element with a path to the text
+    // and the download attribute set to the name of the file
     const element = document.createElement('a');
     element.setAttribute('href', 
     'data:text/plain;charset=utf-8, '
@@ -249,21 +249,24 @@ function download(file, text) {
   
     //onClick property
     element.click();
-  
+  //after download, delete the element we'd created for the download
     document.body.removeChild(element);
     
 }
   
 // Start file download.
 downloadPoem.addEventListener("click", function() {
-    // Generate download of Poem.txt 
-    
+    // Generate download of Poem.txt
+    // Right now the thisTextFormatted is in html
     const html = thisTextFormatted;
+    // turn it into a more readable form by replacing all the 
+    // breaks with new lines
     const text = thisTextFormatted.replace(/ <br> /g, "\n");
     const filename = "Poem.txt";
-    console.log(text);
-    console.log(filename);
+    // send the text with the generic filename to the download function
     download(filename, text);
+    //what is the below 'false' for?
+    // From what I can see, this is set to false by default anyway
 }, false);
 
 
@@ -292,13 +295,12 @@ startOver.addEventListener("click", () => {
 
     //delete the existing poem
     const poemDivToRemove = document.querySelectorAll(".poem-div");
-    console.log(poemDivToRemove);
     poemDivToRemove.innerHTML = "";
-    console.log(poemDivToRemove.innerHTML);
-
+/*
     for (element of poemDivToRemove) {
         element.remove();
     }
+    */
 
     //unhide the author wild card button
     wildAuthor.classList.remove("hide");
